291190e6cb29f6259eeee3c47703a60cc2d2a329
blobsey <me@blobsey.com>
2025-07-12 02:33:07 -0700

    Initial infra commit, defines CloudFront Distribution, S3 bucket, DNS records, Certificate

--- cdk/app.ts
+++ cdk/app.ts
@@ -1,11 +1,31 @@
+import { DomainStack } from './lib/DomainStack';
 import { WebsiteStack } from './lib/WebsiteStack';
 import { App } from 'aws-cdk-lib';
+import * as dotenv from 'dotenv';
+import * as path from 'path';
 
 const app = new App();
+dotenv.config({ path: path.join(__dirname, '../.env') });
+const domainName = process.env.DOMAIN_NAME;
+if (!domainName) {
+  throw new Error('DOMAIN_NAME environment variable is required');
+}
+
+const domainStack = new DomainStack(app, 'DomainStack', {
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: 'us-east-1', // Limitation of CloudFront, need us-east-1
+  },
+  domainName: domainName, 
+});
 
 const websiteStack = new WebsiteStack(app, 'WebsiteStack', {
   env: {
-        account: process.env.CDK_DEFAULT_ACCOUNT,
+    account: process.env.CDK_DEFAULT_ACCOUNT,
     region: process.env.CDK_DEFAULT_REGION,
   },
+  hostedZone: domainStack.hostedZone,
+  certificate: domainStack.certificate,
 });
+
+websiteStack.addDependency(domainStack);
